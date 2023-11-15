c9a39d71067ab820a5b0ac4118581e631f981695
migmoc <miglemoceviciute@gmail.com>
2023-11-15 11:42:22 +0200

    feat(singleton): implement thread-safe singleton pattern

--- packages/unapy/src/Services/CacheService.ts
+++ packages/unapy/src/Services/CacheService.ts
@@ -1,65 +1,69 @@
-import Redis, { Redis as RedisInterface } from "ioredis"
-
-import redisConfig from "@/Config/redis"
+import Redis, { Redis as RedisInterface } from "ioredis";
+import redisConfig from "@/Config/redis";
+import Mutex from "@/Mutex/Mutex";
 
 class CacheService {
-	redis: RedisInterface
+  private static instance: CacheService;
+  private static instanceCreationLock = new Mutex();
+  redis: RedisInterface;
 
-	constructor () {
-		const redisPort = Number(redisConfig.port)
+  private constructor() {
+    const redisPort = Number(redisConfig.port);
 
-		this.redis = new Redis(redisPort, redisConfig.host, {
-			password: redisConfig.password,
-		})
-	}
+    this.redis = new Redis(redisPort, redisConfig.host, {
+      password: redisConfig.password,
+    });
+  }
 
-	async getKeysByPattern (pattern: string): Promise<string[]> {
-		const keys = await new Promise<string[]>((resolve, reject) => {
-			this.redis.keys(pattern, (error, keys) => {
-				if (error) {
-					reject(error)
-				}
+  static getInstance(): CacheService {
+    if (!CacheService.instance) {      
+      CacheService.instanceCreationLock.acquire();
+      try {
+        if (!CacheService.instance) {
+          CacheService.instance = new CacheService();
+        }
+      } finally {
+        CacheService.instanceCreationLock.release();
+      }
+    }
+    return CacheService.instance;
+  }
 
-				resolve(keys)
-			})
-		})
+  async get<ExpectedData extends Record<string, unknown>>(
+    key: string
+  ): Promise<ExpectedData> {
+    const cached = await this.redis.get(key);
 
-		return keys
-	}
+    if (cached) {
+      return JSON.parse(cached);
+    } else {
+      return null;
+    }
+  }
 
-	async get<ExpectedData extends Record<string, unknown>> (key: string): Promise<ExpectedData> {
-		const cached = await this.redis.get(key)
+  async set<ExpectedData extends Record<string, unknown>>(
+    key: string,
+    value: ExpectedData,
+    expiration: number = null
+  ): Promise<boolean> {
+    const payload = JSON.stringify(value);
 
-		if (cached) {
-			return JSON.parse(cached)
-		} else {
-			return null
-		}
-	}
+    if (expiration) {
+      return Boolean(await this.redis.set(key, payload, "EX", expiration));
+    } else {
+      return Boolean(await this.redis.set(key, payload));
+    }
+  }
 
-	async set<ExpectedData extends Record<string, unknown>> (
-		key: string,
-		value: ExpectedData,
-		expiration: number = null,
-	): Promise<boolean> {
-		const payload = JSON.stringify(value)
+  async delete(key: string): Promise<boolean> {
+    const isKeyDeleted = await this.redis.del(key);
 
-		if (expiration) {
-			return Boolean(await this.redis.set(key, payload, "EX", expiration))
-		} else {
-			return Boolean(await this.redis.set(key, payload))
-		}
-	}
-
-	async delete (key: string): Promise<boolean> {
-		const isKeyDeleted = await this.redis.del(key)
-
-		if (isKeyDeleted) {
-			return true
-		} else {
-			return false
-		}
-	}
+    if (isKeyDeleted) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
 
-export default new CacheService()
+export default CacheService.getInstance();
